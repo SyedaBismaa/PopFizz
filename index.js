73f33fd6704237a8b77200ad9c92bcd02c89c7b8
SyedaBismaa <syedabisma173@gmail.com>
2025-04-27 23:03:04 +0530

    Completed

--- index.js
+++ index.js
@@ -1,54 +1,71 @@
-var timer = 60;
-    var score = 0;
-    var hitrn = 0;
+var timer = 30;
+var score = 0;
+var hitrn = 0;
 
-    // Score increase function
-    function increaseScore() {
-        score += 10;
-        document.querySelector("#scoreval").textContent = score;
+// Score increase function
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+// Get new hit value
+function getNewHit() {
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+// Create bubbles
+function makebubble() {
+    var clutter = "";
+
+    for (var i = 1; i <= 180; i++) {
+        var rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`;
     }
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
 
-    // Get new hit value
-    function getNewHit() {
-        hitrn = Math.floor(Math.random() * 10);
-        document.querySelector("#hitval").textContent = hitrn;
-    }
+// Run timer
+function runTimer() {
+    var timeint = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        } else {
+            clearInterval(timeint);
+            document.querySelector("#pbtm").innerHTML = `<h1>GAME OVER <br><br> your Final score is : ${score} <br> <button id="resetBtn">Restart</button> </h1>
+            `;
 
-    // Create bubbles
-    function makebubble() {
-        var clutter = "";
-
-        for (var i = 1; i <= 184; i++) {
-            var rn = Math.floor(Math.random() * 10);
-            clutter += `<div class="bubble">${rn}</div>`;
+            // Add event listener to the reset button
+            document.querySelector("#resetBtn").addEventListener("click", function () {
+                resetGame();
+            });
         }
-        document.querySelector("#pbtm").innerHTML = clutter;
-    }
+    }, 1000);
+}
 
-    // Run timer
-    function runTimer() {
-        var timeint = setInterval(function() {
-            if (timer > 0) {
-                timer--;
-                document.querySelector("#timerval").textContent = timer;
-            } else {
-                clearInterval(timeint);
-                document.querySelector("#pbtm").innerHTML = "<h1>GAME OVER</h1>";
-            }
-        }, 1000);
-    }
-
-    // Click event listener
-    document.querySelector("#pbtm").addEventListener("click", function(dets) {
-        var clickedNumber = Number(dets.target.textContent);
-        if (clickedNumber === hitrn) {
-            increaseScore();
-            makebubble();
-            getNewHit();
-        }
-    });
-
-    // Initial function calls
-    runTimer();
+// Reset function
+function resetGame() {
+    timer = 30;
+    score = 0;
+    document.querySelector("#timerval").textContent = timer;
+    document.querySelector("#scoreval").textContent = score;
     makebubble();
     getNewHit();
+    runTimer();
+}
+
+// Click event listener
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+    var clickedNumber = Number(dets.target.textContent);
+    if (clickedNumber === hitrn) {
+        increaseScore();
+        makebubble();
+        getNewHit();
+    }
+});
+
+// Initial function calls
+runTimer();
+makebubble();
+getNewHit();
